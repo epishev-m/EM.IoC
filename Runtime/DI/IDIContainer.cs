cfc15a7a69e1e8006086e4132299d42aed5b673a
Maxim Epishev <epishev.m@mail.ru>
2020-11-02 22:37:20 +0300

    Added IDIContainer

---
+++ Runtime/DI/IDIContainer.cs
@@ -0,0 +1,20 @@
+﻿
+namespace EM.IoC
+{
+	using System;
+	
+	public interface IDIContainer
+	{
+		object GetInstance(
+			Type type);
+
+		T GetInstance<T>()
+			where T : class;
+
+		IDIBinding Bind<T>()
+			where T : class;
+
+		void Unbind<T>()
+			where T : class;
+	}
+}

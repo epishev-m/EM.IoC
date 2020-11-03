4f52542af5ea06451095297dc62aaf64dba42eb0
Maxim Epishev <epishev.m@mail.ru>
2020-11-03 23:48:41 +0300

    Added ICommandBinding

---
+++ Runtime/CommandBinder/ICommandBinding.cs
@@ -0,0 +1,20 @@
+﻿
+namespace EM.IoC
+{
+	using EM.Foundation;
+
+	public interface ICommandBinding
+	{
+		bool IsSequence
+		{
+			get;
+		}
+
+		ICommandBinding InParallel();
+
+		ICommandBinding InSequence();
+
+		ICommandBinding To<T>()
+			where T : ICommand;
+	}
+}

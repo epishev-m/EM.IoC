35b49bb637c42f99312c3874742d21983457dc6a
Maxim Epishev <epishev.m@mail.ru>
2020-11-07 21:25:01 +0300

    Added CommandContainer

---
+++ Runtime/CommandContainer/CommandContainer.cs
@@ -0,0 +1,160 @@
+﻿
+namespace EM.IoC
+{
+	using EM.Foundation;
+	using System;
+	using System.Collections.Generic;
+
+	public class CommandContainer :
+		Binder,
+		ICommandContainer
+	{
+		#region ICommandContainer
+
+		public ICommandBindingComposite Bind<T>()
+		{
+			return base.Bind<T>() as ICommandBindingComposite;
+		}
+
+		public ICommandBindingComposite Bind(
+			object key)
+		{
+			return base.Bind(key) as ICommandBindingComposite;
+		}
+
+		public void ReactTo<T>(
+			object data = null)
+		{
+			ReactTo(typeof(T), data);
+		}
+
+		public void ReactTo(
+			object trigger,
+			object data = null)
+		{
+			if (GetBinding(trigger) is CommandBinding binding)
+			{
+				var composite = (bool) binding.IsSequence ? GetSequence() : GetBatch();
+				var commandTypesArray = binding.Values;
+
+				foreach (var type in commandTypesArray)
+				{
+					var command = GetCommand(type as Type);
+					command.Data = data;
+					composite.Add(command);
+				}
+
+				composite.Done += () => PutComposite(composite, (bool) binding.IsSequence);
+				composite.Execute();
+			}
+		}
+
+		public bool Unbind<T>()
+		{
+			return base.Unbind<T>();
+		}
+
+		public bool Unbind(
+			object key)
+		{
+			return base.Unbind(key);
+		}
+
+		#endregion
+		#region Binder
+
+		protected override IBinding GetRawBinding(
+			object key,
+			object name)
+		{
+			return new CommandBinding(this, key, name, BindingResolver);
+		}
+
+		#endregion
+		#region CommandContainer
+
+		private readonly IDIContainer container;
+
+		protected readonly Pool<CommandSequence> poolSequences;
+
+		protected readonly Pool<CommandBatch> poolBatches;
+
+		protected readonly Dictionary<Type, Pool<ICommand>> poolCommands;
+
+		public CommandContainer(
+			IDIContainer container)
+		{
+			Requires.IsNotNull(container, nameof(container));
+
+			this.container = container;
+			poolSequences = new Pool<CommandSequence>();
+			poolBatches = new Pool<CommandBatch>();
+			poolCommands = new Dictionary<Type, Pool<ICommand>>();
+		}
+
+		private void PutComposite(
+			ICommandComposite composite,
+			bool isSequence)
+		{
+			PutCommands(composite.Commands);
+
+			if (isSequence)
+			{
+				poolSequences.PutObject(composite as CommandSequence);
+			}
+			else
+			{
+				poolBatches.PutObject(composite as CommandBatch);
+			}
+		}
+
+		private ICommandComposite GetSequence()
+		{
+			var sequence = poolSequences.GetObject();
+			sequence = sequence ?? new CommandSequence();
+
+			return sequence;
+		}
+
+		private ICommandComposite GetBatch()
+		{
+			var batch = poolBatches.GetObject();
+			batch = batch ?? new CommandBatch();
+
+			return batch;
+		}
+
+		private ICommand GetCommand(
+			Type commandType)
+		{
+			var command = default(ICommand);
+
+			if (poolCommands.TryGetValue(commandType, out var pool))
+			{
+				command = pool.GetObject();
+			}
+			else
+			{
+				pool = new Pool<ICommand>();
+				poolCommands.Add(commandType, pool);
+			}
+
+			command = command ?? container.GetInstance(commandType) as ICommand;
+
+			return command;
+		}
+
+		private void PutCommands(
+			IEnumerable<ICommand> commands)
+		{
+			foreach (var command in commands)
+			{
+				var commandType = command.GetType();
+				var pool = poolCommands[commandType];
+				pool.PutObject(command);
+			}
+		}
+
+		#endregion
+	}
+}
---
+++ Runtime/CommandContainer/ICommandContainer.cs
@@ -0,0 +1,23 @@
+﻿
+namespace EM.IoC
+{
+	public interface ICommandContainer
+	{
+		void ReactTo<T>(
+			object data = null);
+
+		void ReactTo(
+			object trigger,
+			object data = null);
+
+		ICommandBindingComposite Bind<T>();
+
+		ICommandBindingComposite Bind(
+			object key);
+
+		bool Unbind<T>();
+
+		bool Unbind(
+			object key);
+	}
+}

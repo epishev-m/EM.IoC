7b0dc54d88cede9757a4b33eca5915671e6f84b5
Maxim Epishev <epishev.m@mail.ru>
2020-11-10 21:32:48 +0300

    Added SignalCommandContainer

---
+++ Runtime/SignalCommandContainer/ISignalCommandContainer.cs
@@ -0,0 +1,14 @@
+﻿
+namespace EM.IoC
+{
+	using EM.Foundation;
+
+	public interface ISignalCommandContainer
+	{
+		ISignalCommandBindingComposite Bind<T>()
+			where T : class, ISignal;
+
+		bool Unbind<T>()
+			where T : class, ISignal;
+	}
+}
---
+++ Runtime/SignalCommandContainer/SignalCommandContainer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace EM.IoC
+{
+	using EM.Foundation;
+
+	public sealed class SignalCommandContainer :
+		CommandContainer,
+		ISignalCommandContainer
+	{
+		#region ISignalCommandContainer
+
+		public new ISignalCommandBindingComposite Bind<T>()
+			where T : class, ISignal 
+		{
+			var signal = container.GetInstance<T>();
+
+			return Bind(signal) as ISignalCommandBindingComposite;
+		}
+
+		public new bool Unbind<T>()
+			where T : class, ISignal
+		{
+			var signal = container.GetInstance<T>();
+			signal.RemoveListener(ReactTo);
+
+			return Unbind(signal);
+		}
+
+		#endregion
+		#region CommandContainer
+
+		public override void ReactTo(
+			object trigger,
+			object data)
+		{
+			base.ReactTo(trigger, data);
+
+			if (GetBinding(trigger) is SignalCommandBinding binding)
+			{
+				if (binding.IsOneOff)
+				{
+					Unbind(binding);
+				}
+			}
+		}
+
+		protected override IBinding GetRawBinding(
+			object key,
+			object name)
+		{
+			return new SignalCommandBinding(this, key, name, BindingResolver);
+		}
+
+		protected override void BindingResolver(IBinding binding)
+		{
+			base.BindingResolver(binding);
+
+			var signal = binding.Key as ISignal;
+			signal.AddListener(ReactTo);
+		}
+
+		#endregion
+		#region SignalCommandContainer
+
+		public SignalCommandContainer(
+			IDIContainer container) :
+			base(container)
+		{
+		}
+
+		#endregion
+	}
+}
